d8784cab0d637951ecbe0397d758d3a5a3ae8bc6
winniewandia <winniewandia63@gmail.com>
2023-08-30 11:23:31 +0300

    depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * recursive_depth - measures the depth of a node in a binary tree
+ * @tree: a pointer to the node to measure the depth
+ *
+ * Return: depth of the node and 0 if tree is NULL
+ */
+size_t recursive_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+	return (recursive_depth(tree->parent) + 1);
+}
+
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: a pointer to the node to measure the depth
+ *
+ * Return: depth of the node and 0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (recursive_depth(tree));
+}
--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t recursive_height(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t recursive_depth(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 #endif
