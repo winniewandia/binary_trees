5375d864a0182f8a680cfe163210b6c6c307a5fb
winniewandia <winniewandia63@gmail.com>
2023-08-30 11:32:16 +0300

    Size

--- binary_trees.h
+++ binary_trees.h
@@ -38,5 +38,6 @@ size_t recursive_height(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t recursive_depth(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif
