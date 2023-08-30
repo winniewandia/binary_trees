098731dbea9075d77846181f83a4b2b9df022b0d
winniewandia <winniewandia63@gmail.com>
2023-08-30 11:34:21 +0300

    binary tree size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,9 +8,8 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size;
-
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (binary_tree_size(tree->left) +
+		binary_tree_size(tree->right) + 1);
 }
